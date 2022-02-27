0e964b61fc2fff31cf782ce324a1da8cdab6dcf3
mfodat <69844255+mfodat@users.noreply.github.com>
2022-02-27 08:23:21 +0300

    intialzing

---
+++ car_rental/car_rantal/src/main/java/com/belal/car_rantal/App.java
@@ -0,0 +1,13 @@
+package com.belal.car_rantal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
